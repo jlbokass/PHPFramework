636eaf7451abad0902138a041a1651c2c30bd7f8
Jean le Grand Bokassa <jean.le.grand.bokassa@gmail.com>
2022-08-10 18:59:16 +0200

    added ajax call to verify if email exist

---
+++ App/Controllers/Account.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\User;
+use Core\Controller;
+
+/**
+ * Account controller
+ *
+ * PHP 7.4
+ */
+
+class Account extends Controller
+{
+    /**
+     * Validate if email is available with ajax request for new signup
+     *
+     * @return void
+     */
+    public function validateEmailAction(): void
+    {
+        $is_valid = ! User::emailExists($_GET['email']);
+
+        header('Content-Type: application/json');
+        echo json_encode($is_valid);
+    }
+}
