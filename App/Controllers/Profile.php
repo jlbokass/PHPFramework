d3595ed89efa03b4c4fba69a2973cb82cc7db409
Jean le Grand Bokassa <jean.le.grand.bokassa@gmail.com>
2022-08-15 05:45:01 +0200

    Features profile

---
+++ App/Controllers/Profile.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Auth;
+use App\Flash;
+use Core\View;
+
+/**
+ * Profile controller
+ *
+ * PHP 7.4
+ */
+class Profile extends Authenticated
+{
+    /**
+     * Before filter - called before each action method
+     *
+     * @return void
+     */
+    public function before()
+    {
+        parent::before();
+        $this->user = Auth::getUser();
+    }
+
+    /**
+     * Show the profile
+     *
+     * @return void
+     */
+    public function showAction()
+    {
+        View::renderTemplate('Profile/show.html.twig', [
+            'user' => $this->user,
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function editAction()
+    {
+        View::renderTemplate('Profile/edit.html.twig', [
+            'user' => $this->user,
+        ]);
+    }
+
+    /**
+     * Update the profile
+     *
+     * @return void
+     */
+    public function updateAction()
+    {
+        if ($this->user->updateProfile($_POST)) {
+
+            Flash::addMessage('Changes saved');
+
+            $this->redirect('/PHPFramework/profile/show');
+
+        } else {
+
+            View::renderTemplate('Profile/edit.html.twig', [
+                'user' => $this->user
+            ]);
+
+        }
+    }
+}
